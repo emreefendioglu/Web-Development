4bf5aaab15235c5e3e1e53efd7181825824907fd
Emre Efendioğlu <emre@unicrow.com>
2021-12-26 13:07:52 +0300

    index html gösterildi

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const requestListener = function (req, res) {
+    const index = fs.readFileSync("./index.html");
     res.writeHead(200);
-    res.end("Hello World");
+    res.end(index.toString());
 }
 
 const server = http.createServer(requestListener);

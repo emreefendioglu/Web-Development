11d925ac8e157b72f1a087eb2dc522783a82102d
Emre Efendioğlu <emre@unicrow.com>
2021-12-26 13:19:41 +0300

    basit route

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const http = require('http');
 const fs = require('fs');
 
+
 const requestListener = function (req, res) {
-    const index = fs.readFileSync("./index.html");
     res.writeHead(200);
-    res.end(index.toString());
+    if(req.url == "/data.json") {
+        const json = fs.readFileSync("./data.json");
+        res.end(json.toString());
+    } else {
+        const index = fs.readFileSync("./index.html");
+        res.end(index.toString());
+    }
 }
 
 const server = http.createServer(requestListener);
